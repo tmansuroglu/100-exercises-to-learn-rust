da52d4cb5e94a679eb44ccc42f88f93cb519595b
tmansuroglu <mansuroglu99@gmail.com>
2024-12-11 22:14:10 +0300

    solved 03 12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,3 +1,82 @@
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn is_product_name_valid (name: &str) -> bool {
+        if name.is_empty() {
+            panic!("Invalid product name")
+        }
+
+        if name.len() >300 {
+            panic!("Invalid product name")
+        }
+        
+        true
+    }
+    pub fn is_quantity_valid (quantity: u32) -> bool {
+        if quantity == 0  {
+            panic!("Invalid quantity")
+        }
+
+        true
+    }
+    pub fn is_unit_price_valid (price: u32) -> bool {
+        if price == 0 {
+            panic!("Invalid unit price")
+        }
+
+        true
+    }
+
+    pub fn total (&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name (&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn unit_price (&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn quantity (&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn new(product_name: String,
+        quantity: u32,
+        unit_price: u32,) -> Order {
+
+            Order::is_product_name_valid(&product_name);
+            Order::is_quantity_valid(quantity);
+            Order::is_unit_price_valid(unit_price);
+
+            Order {
+                product_name,
+                quantity,
+                unit_price,
+            }
+        }
+
+        pub fn set_product_name( &mut self, new_name:String){
+            Order::is_product_name_valid(&new_name);
+            self.product_name = new_name;
+        }
+        pub fn set_quantity(&mut self, new_quantity:u32){
+            Order::is_quantity_valid(new_quantity);
+            self.quantity = new_quantity;
+        }
+        pub fn set_unit_price(&mut self, new_unit_price:u32){
+            Order::is_unit_price_valid(new_unit_price);
+            self.unit_price = new_unit_price;
+        }
+
+}
+
 // TODO: Define a new `Order` type.
 //   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
 //   The product name can't be empty and it can't be longer than 300 bytes.
